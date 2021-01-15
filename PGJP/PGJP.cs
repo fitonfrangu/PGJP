65fcc9d2f0c1ef1614476f9b8ba135de42dfa3e9
Fiton Frangu <fitonfrangi@gmail.com>
2021-01-15 16:05:30 +0100

    Add project files.

---
+++ PGJP/PGJP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PGJP
+{
+    class PGJP
+    {
+        static string ParashtesaPerbashketHelper(string stringu1, string stringu2)
+        {
+            string result = "";
+
+            int n1 = stringu1.Length,
+                n2 = stringu2.Length;
+
+            for (int i = 0, j = 0; i <= n1 - 1 && j <= n2 - 1; i++, j++)
+            {
+                if (stringu1[i] != stringu2[j])
+                    break;
+
+                result += stringu1[i];
+            }
+
+            return (result);
+        }
+
+        public static string parashtesaPerbashket(string[] stringArray, int l, int r)
+        {
+            if (l == r)
+                return (stringArray[l]);
+
+            if (r > l)
+            {
+                int mid = (l + r) / 2;
+
+                string stringu1 = parashtesaPerbashket(stringArray, l, mid);
+                string stringu2 = parashtesaPerbashket(stringArray, mid + 1, r);
+
+                return (ParashtesaPerbashketHelper(stringu1, stringu2));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PGJP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PGJP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] stringArray = { "amerika", "amerikani", "amerikanizmi" };
+            
+
+        int n = stringArray.Length;
+
+            String parashtesa = PGJP.parashtesaPerbashket(stringArray, 0, n - 1);
+
+            if (parashtesa.Length != 0)
+            {
+                Console.Write("Parashtesa me e gjate e perbashket eshte " + parashtesa);
+
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write("Nuk ka parashtese te perbashket");
+            }
+
+
+        }
+    }
+}
